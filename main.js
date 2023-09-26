fa508166069a38c45da051e4b80f3192a1631db3
Mohamad Mourad <89941937+Mohamadmourad@users.noreply.github.com>
2023-09-26 09:17:53 +0300

    pause and reset buuton and design

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let seconds1 = 0;
 let minutes1 = 3;
 let seconds = 0;
 let minutes = 3;
-
+let timeArray = [3,0]//minutes , seconds;
 
 let clickCount0 = 0;
 let clickCount1 = 0;
@@ -12,6 +12,7 @@ let interval0 = '';
 let interval1 = '';
 let turnCount = 0;
 let turn = 0;
+let pauseCounter = 0;
 
 let secondNow = 0;
 let minuteNow = 0;
@@ -20,6 +21,7 @@ let clickCountNow = 0;
 
 
 document.getElementById('button1').onclick = () => {
+    seconds0 += timeArray[1];
     interval0 = setInterval(clock,1000);
     clearInterval(interval1);
     whichTurn();
@@ -28,11 +30,69 @@ document.getElementById('button1').onclick = () => {
 }
 
 document.getElementById('button2').onclick = () => {
+    seconds1 += timeArray[1];
     interval1 = setInterval(clock,1000);
     clearInterval(interval0);
     whichTurn();
     document.getElementById('button2').disabled = true;
     document.getElementById('button1').disabled = false;
+}
+
+document.getElementById('timeControle').onchange = () => {
+    let time = document.getElementById('timeControle').value;
+    let timeArray = timeControle(time);
+    document.getElementById('minute0').innerHTML = timeArray[0];
+    document.getElementById('minute1').innerHTML = timeArray[0];
+    document.getElementById('second0').innerHTML = timeArray[1] .toString().padStart(2, '0');
+    document.getElementById('second1').innerHTML = timeArray[1] .toString().padStart(2, '0');
+     seconds0 = timeArray[1];
+ minutes0 = timeArray[0];
+ seconds1 = timeArray[1];
+ minutes1 = timeArray[0];
+ seconds = timeArray[1];
+ minutes = timeArray[0];
+}
+
+document.getElementById('pause').onclick = () => {
+if(pauseCounter%2 == 0){
+    clearInterval(interval0);
+    clearInterval(interval1);
+    document.getElementById('pause').classList.remove("fa-pause");
+    document.getElementById('pause').classList.add("fa-play")
+}
+else{
+    document.getElementById('pause').classList.add("fa-pause");
+    document.getElementById('pause').classList.remove("fa-play")
+    if(turn == 0)
+    interval0 = setInterval(clock,1000);
+else
+interval1 = setInterval(clock,1000);
+}
+pauseCounter++;
+}
+
+document.getElementById('reset').onclick = () => {
+    clearInterval(interval0);
+    clearInterval(interval1);
+    document.getElementById('minute0').innerHTML = timeArray[0];
+    document.getElementById('minute1').innerHTML = timeArray[0];
+    document.getElementById('second0').innerHTML = timeArray[1] .toString().padStart(2, '0');
+    document.getElementById('second1').innerHTML = timeArray[1] .toString().padStart(2, '0');
+    seconds0 = timeArray[1];
+    minutes0 = timeArray[0];
+    seconds1 = timeArray[1];
+    minutes1 = timeArray[0];
+    seconds = timeArray[1];
+    minutes = timeArray[0];
+    document.getElementById('button1').disabled = false;
+    document.getElementById('button2').disabled = false;
+     clickCount0 = 0;
+ clickCount1 = 0;
+ interval0 = '';
+ interval1 = '';
+ turnCount = 0;
+ turn = 0;
+ pauseCounter = 0;
 }
 
 
@@ -92,3 +152,18 @@ function whichTurn(){
     turnCount++;
 }
 
+function timeControle(input){
+    if(input == 30)
+    timeArray = [3,0]
+    else if(input == 32)
+    timeArray = [3,2]
+    else if(input == 50)
+    timeArray = [5,0]
+    else if(input == 53)
+    timeArray = [5,3]
+    else if(input == 100)
+    timeArray = [10,0]
+    else if(input == 105)
+    timeArray = [10,5]
+    return timeArray;
+}
